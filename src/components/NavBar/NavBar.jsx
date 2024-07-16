7b4c9657923e888c8af84e3cf4bcd2cf385286b4
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-07-16 14:06:52 +0300

    Add navigation paths in NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,14 @@ import ThemeChangeIcons from '../ThemeChangeIcons/ThemeChangeIcons';
 import { DARK_THEME, LIGHT_THEME } from '../../common/constants';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  AiOutlineContacts,
+  AiOutlineFundProjectionScreen,
+  AiOutlineHome,
+  AiOutlineTool,
+  AiOutlineUser,
+} from 'react-icons/ai';
 
 export default function NavBar() {
   const [theme, setTheme] = useState(
@@ -18,11 +26,47 @@ export default function NavBar() {
   }, [theme]);
 
   return (
-    <div>
-      <ThemeChangeIcons
-        toggleTheme={handleToggle}
-        currentTheme={theme}
-      />
+    <div className='drawer bg-base-300 flex justify-between px-3 items-center'>
+      <div>
+        <ThemeChangeIcons
+          toggleTheme={handleToggle}
+          currentTheme={theme}
+        />
+      </div>
+      <div>
+        <ul className='menu menu-horizontal items-center text-lg'>
+          <li>
+            <NavLink to='/'>
+              <AiOutlineHome />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/about'>
+              <AiOutlineUser />
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/skills'>
+              <AiOutlineTool />
+              Skills
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/projects'>
+              <AiOutlineFundProjectionScreen />
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/contact'>
+              <AiOutlineContacts />
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
