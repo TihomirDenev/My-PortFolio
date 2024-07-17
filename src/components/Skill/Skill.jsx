6d251872d92b5b2052d4bef2b3fa58965a070d03
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-07-17 18:26:12 +0300

    Create Skill component

---
+++ src/components/Skill/Skill.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export default function Skill({ icon, title, description }) {
+  return (
+    <div className='flex flex-col items-center border-2 border-gray-500 rounded-2xl w-36 bg-orange-300 m-2 duration-300 hover:-translate-y-2 hover:shadow-2xl'>
+      <div className='m-1'>{icon}</div>
+      <p className='text-base font-bold'>{title}</p>
+      <p className='text-sm text-center text-black px-2 pb-3'>{description}</p>
+    </div>
+  );
+}
+
+Skill.propTypes = {
+  icon: PropTypes.elementType.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
