d381555d0f8f4e0b917c5f28cb7678fd4a503cce
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-07-17 18:26:40 +0300

    Add content to Contact page

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,7 +1,24 @@
+import Connect from '../../components/Connect/Connect';
+import { IoIosArrowRoundDown } from 'react-icons/io';
+import avatar from '../../assets/avatar.svg';
+
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className='flex flex-row items-center my-8 mx-28'>
+      <img
+        src={avatar}
+        alt='Avatar'
+      />
+      <div className='flex flex-col items-center ml-10'>
+        <h1 className='text-2xl font-bold underline'>Contact: </h1>
+        <p className='text-center text-lg m-2'>
+          If you`re interested in business inquiries, collaborations, or any
+          professional opportunities, please reach out using the contact details
+          provided.
+        </p>
+        <IoIosArrowRoundDown className='text-5xl my-2' />
+        <Connect />
+      </div>
     </div>
   );
 }
