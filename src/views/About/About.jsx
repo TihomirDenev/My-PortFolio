a084ef3bd3448bccce92ac983e9233aa7b8db99e
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-07-22 19:05:59 +0300

    Update About Section Content

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,11 +1,15 @@
 import image from '../../assets/about.png';
+import frontImg from '../../assets/front.png';
+import backImg from '../../assets/back.png';
 
 export default function About() {
   return (
-    <div className='flex flex-col items-center my-8'>
-      <h1 className='text-2xl font-bold underline'>Let me introduce myself:</h1>
+    <div className='flex flex-col items-center my-6 text-lg'>
+      <h1 className='text-2xl font-bold underline'>
+        👦🏻 Let me introduce myself:
+      </h1>
       <div className='flex flex-row items-center w-10/12 mt-4'>
-        <div className='text-center text-lg mt-2'>
+        <div className='text-center mt-2'>
           <p>
             I fell in love with programming and I have at least learnt
             something, I think… 🤷‍♂️
@@ -18,21 +22,6 @@ export default function About() {
             libraries and frameworks like React.js.
           </p>
           <br />
-          <p>Apart from coding, I enjoy several other activities:</p>
-
-          <p>🏋🏻‍♂️ Training</p>
-          <p>🎒 Traveling</p>
-          <p>🎼 Listen Music</p>
-          <p>🧮 Play Board Games</p>
-          <p>🎥 Watching Good Movies</p>
-          <p>🎓 Studying New Technology</p>
-          <br />
-          <p className='text-primary text-base'>
-            <i>
-              {`"Approach every task with a positive attitude and a smile,
-              striving to improve and excel each time." -TDD`}
-            </i>
-          </p>
         </div>
         <div>
           <img
@@ -40,6 +29,66 @@ export default function About() {
             alt='about'
           />
         </div>
+      </div>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-2xl font-bold underline'>
+          🎓 Education and Other Interests:
+        </h1>
+        <div className='flex items-center text-center mt-8'>
+          <div className='mr-10'>
+            <p>Apart from coding, I enjoy several other activities:</p>
+            <p>🏋🏻‍♂️ Training</p>
+            <p>🎒 Traveling</p>
+            <p>🎼 Listen Music</p>
+            <p>🧮 Play Board Games</p>
+            <p>🎥 Watching Good Movies</p>
+            <p>🎓 Studying New Technology</p>
+          </div>
+          <div>
+            <p className='mb-2'>When it comes to my educational background:</p>
+            <p className='mb-2'>
+              📒 Telerik Academy: Completed Alpha JavaScript Track.{' '}
+            </p>
+            <p className='mb-2'>
+              📘 Technical University of Sofia: Bachelor of Automatic.{' '}
+            </p>{' '}
+            <p className='mb-2'>
+              📕 Science and Mathematics High School: Informatics.
+            </p>
+          </div>
+        </div>
+        <br />
+        <h1 className='text-2xl font-bold underline'>📑 Certificate: </h1>
+        <div className='flex mt-8'>
+          <a
+            href={frontImg}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={frontImg}
+              alt='front'
+              className='h-96 mr-5 border-2 border-black shadow-2xl duration-300 hover:-translate-y-1'
+            />
+          </a>
+          <a
+            href={backImg}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={backImg}
+              alt='back'
+              className='h-96 border-2 border-black shadow-2xl duration-300 hover:-translate-y-1'
+            />
+          </a>
+        </div>
+        <p className='text-primary text-center text-base mt-6'>
+          <i>
+            {`"Approach every task with a positive attitude and a smile,
+              striving to improve and excel each time." -TDD`}
+          </i>
+        </p>
       </div>
     </div>
   );
