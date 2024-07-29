acf1ddc7f27a3227ed3534fd33dcd0dab7c45434
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-07-29 21:24:24 +0300

    Rename skill.jsx to SkillCard.jsx for better clarity

--- src/components/SkillCard/SkillCard.jsx
+++ src/components/SkillCard/SkillCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-export default function Skill({ icon, title, description }) {
+export default function SkillCard({ icon, title, description }) {
   return (
     <div className='flex flex-col items-center border-2 border-gray-500 rounded-2xl w-36 bg-orange-300 m-2 duration-300 hover:-translate-y-2 hover:shadow-2xl'>
       <div className='m-1'>{icon}</div>
@@ -10,7 +10,7 @@ export default function Skill({ icon, title, description }) {
   );
 }
 
-Skill.propTypes = {
+SkillCard.propTypes = {
   icon: PropTypes.node.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
