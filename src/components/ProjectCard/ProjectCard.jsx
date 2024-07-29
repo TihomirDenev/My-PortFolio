af27871e8d43ce51b6f17633237b6a69e39753f4
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-07-29 21:26:15 +0300

    Create ProjectCard.jsx component

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export default function ProjectCard({ title, description, link, image }) {
+  return (
+    <div className='flex flex-col p-4 items-center border-2 border-gray-500 rounded-2xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl m-2 duration-300 hover:-translate-y-1 hover:shadow-2xl'>
+      <img
+        src={image}
+        alt={title}
+        className='h-full md:h-full lg:h-full rounded-md object-cover mb-2'
+      />
+      <p className='text-lg md:text-xl font-bold my-2 text-primary'>{title}</p>
+      <p className='text-sm md:text-base text-center px-2 pb-3 whitespace-pre-line'>
+        {description}
+      </p>
+      <button
+        onClick={() => window.open(link, '_blank')}
+        className='btn btn-accent my-3'
+      >
+        View Project
+      </button>
+    </div>
+  );
+}
+
+ProjectCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
