ee833feae2a48d5b5e2576336e8247f079d615be
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-07-29 21:17:56 +0300

    Center footer text for improved layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import Connect from '../Connect/Connect';
 export default function Footer() {
   return (
     <footer className='flex flex-col md:flex-row justify-between items-center w-full p-4 bg-base-300 text-center md:text-left'>
-      <h3 className='flex-1 mb-2 md:mb-0'>
+      <h3 className='flex-1 mb-2 md:mb-0 text-center md:text-left'>
         Designed and Developed by Tihomir Denev
       </h3>
-      <h3 className='flex-1 mb-2 md:mb-0'>
+      <h3 className='flex-1 mb-2 md:mb-0 text-center'>
         Copyright © 2024 - All rights reserved for TDD
       </h3>
       <div className='flex-1'>
