3e6a93a6ec13a69fe0901a2aad4936691d859cba
Tihomir Denev <tdenev22@abv.bg>
2024-10-08 12:58:13 +0300

    Add filter by in skills section

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export default function ProjectCard({
   title,
@@ -9,42 +9,42 @@ export default function ProjectCard({
   image,
 }) {
   return (
-    <div className='flex flex-col bg-gradient-to-br from-blue-500 via-indigo-500 to-purple-600 p-6 border-2 border-black rounded-3xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl m-4 shadow-lg transform transition-transform duration-300'>
+    <div className="flex flex-col bg-gradient-to-br from-blue-500 via-indigo-500 to-purple-600 p-6 border-2 border-black rounded-3xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl m-4 shadow-lg transform transition-transform duration-300">
       {linkForSite ? (
         <button
-          onClick={() => window.open(linkForSite, '_blank')}
-          className='relative'
+          onClick={() => window.open(linkForSite, "_blank")}
+          className="relative"
         >
           <img
             src={image}
             alt={title}
-            className='w-full rounded-md object-cover mb-2 border-4 border-black transition-transform duration-300 ease-in-out transform hover:scale-105'
+            className="w-full rounded-md object-cover mb-2 border-4 border-black transition-transform duration-300 ease-in-out transform hover:scale-105"
           />
         </button>
       ) : (
         <img
           src={image}
           alt={title}
-          className='w-full rounded-md object-cover mb-2 border-4 border-black transition-transform duration-300 ease-in-out transform hover:scale-105 '
+          className="w-full rounded-md object-cover mb-2 border-4 border-black transition-transform duration-300 ease-in-out transform hover:scale-105 "
         />
       )}
 
-      <p className='text-xl font-bold text-white z-10 text-center'>{title}</p>
+      <p className="text-xl font-bold text-white z-10 text-center">{title}</p>
 
-      <p className='text-sm text-white text-center px-4 py-2 z-10 opacity-90'>
-        <strong className='text-base'>Description: </strong>
+      <p className="text-sm text-white text-center px-4 py-2 z-10 opacity-90">
+        <strong className="text-base">Description: </strong>
         {description}
       </p>
 
-      <p className='text-sm text-white text-center px-4 py-2 z-10 opacity-90'>
-        <strong className='text-base'>Tech Stack: </strong>
+      <p className="text-sm text-white text-center px-4 py-2 z-10 opacity-90">
+        <strong className="text-base">Tech Stack: </strong>
         {techStack}.
       </p>
 
-      <div className='mt-auto w-full flex justify-center'>
+      <div className="mt-auto w-full flex justify-center">
         <button
-          onClick={() => window.open(linkForRepo, '_blank')}
-          className='relative z-10 mt-4 py-2 px-6 bg-white text-gray-800 font-semibold rounded-lg transition-colors duration-300 hover:bg-gray-200'
+          onClick={() => window.open(linkForRepo, "_blank")}
+          className="relative z-10 mt-4 py-2 px-6 bg-white text-gray-800 font-semibold rounded-lg transition-colors duration-300 hover:bg-gray-200"
         >
           Github Repository
         </button>
@@ -58,6 +58,6 @@ ProjectCard.propTypes = {
   description: PropTypes.string.isRequired,
   techStack: PropTypes.string.isRequired,
   linkForRepo: PropTypes.string.isRequired,
-  linkForSite: PropTypes.string.isRequired,
+  linkForSite: PropTypes.string,
   image: PropTypes.string.isRequired,
 };
--- src/views/Skills/Skills.jsx
+++ src/views/Skills/Skills.jsx
@@ -1,54 +1,113 @@
-import skillData from '../../common/skillsData';
-import SkillCard from '../../components/SkillCard/SkillCard';
+import { useState } from "react";
+import skillData from "../../common/skillsData";
+import SkillCard from "../../components/SkillCard/SkillCard";
 
 export default function Skills() {
+  const [showProfessional, setShowProfessional] = useState(true);
+  const [showTools, setShowTools] = useState(true);
+  const [showSoftSkills, setShowSoftSkills] = useState(true);
+
+  const handleToggleCategory = (category) => {
+    if (category === "professional") {
+      setShowProfessional(true);
+      setShowTools(false);
+      setShowSoftSkills(false);
+    } else if (category === "tools") {
+      setShowProfessional(false);
+      setShowTools(true);
+      setShowSoftSkills(false);
+    } else if (category === "softSkills") {
+      setShowProfessional(false);
+      setShowTools(false);
+      setShowSoftSkills(true);
+    }
+  };
+
+  const handleClearFilters = () => {
+    setShowProfessional(true);
+    setShowTools(true);
+    setShowSoftSkills(true);
+  };
+
   return (
-    <div className='flex flex-col items-center my-8 px-4 md:px-0'>
-      <article className='flex flex-col items-center'>
-        <h1 className='text-2xl font-bold underline mb-6'>
-          Professional Skill Set:
-        </h1>
-        <div className='text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
-          {skillData.professionalSkills.map((skill) => (
-            <SkillCard
-              key={skill.title}
-              icon={skill.icon}
-              title={skill.title}
-              description={skill.description}
-              link={skill.link}
-            />
-          ))}
-        </div>
-      </article>
+    <div className="flex flex-col items-center m-8 md:px-0">
+      <ul className="flex flex-row menu bg-gray-800 text-gray-300 text-xl rounded-xl w">
+        <li className="menu-title text-gray-400 text-xl">Filter By:</li>
+        <li>
+          <button onClick={() => handleToggleCategory("professional")}>
+            Skill Set
+          </button>
+        </li>
+        <li>
+          <button onClick={() => handleToggleCategory("tools")}>Tools</button>
+        </li>
+        <li>
+          <button onClick={() => handleToggleCategory("softSkills")}>
+            Soft Skills
+          </button>
+        </li>
+        <li>
+          <button
+            className="hover:bg-gray-500"
+            onClick={handleClearFilters}
+            title="Click to remove all filters and show all skills"
+          >
+            Show All
+          </button>
+        </li>
+      </ul>
 
-      <article className='flex flex-col items-center'>
-        <h1 className='text-2xl font-bold underline my-6'>Tools I Use:</h1>
-        <div className='text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
-          {skillData.tools.map((tool) => (
-            <SkillCard
-              key={tool.title}
-              icon={tool.icon}
-              title={tool.title}
-              description={tool.description}
-              link={tool.link}
-            />
-          ))}
-        </div>
-      </article>
+      {showProfessional && (
+        <article className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold underline my-6">
+            Professional Skill Set:
+          </h1>
+          <div className="text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {skillData.professionalSkills.map((skill) => (
+              <SkillCard
+                key={skill.title}
+                icon={skill.icon}
+                title={skill.title}
+                description={skill.description}
+                link={skill.link}
+              />
+            ))}
+          </div>
+        </article>
+      )}
 
-      <article className='flex flex-col items-center'>
-        <h1 className='text-2xl font-bold underline my-6'>Soft Skills:</h1>
-        <div className='text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
-          {skillData.softSkills.map((softSkill) => (
-            <SkillCard
-              key={softSkill.title}
-              icon={softSkill.icon}
-              title={softSkill.title}
-              description={softSkill.description}
-            />
-          ))}
-        </div>
-      </article>
+      {showTools && (
+        <article className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold underline my-6">Tools I Use:</h1>
+          <div className="text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {skillData.tools.map((tool) => (
+              <SkillCard
+                key={tool.title}
+                icon={tool.icon}
+                title={tool.title}
+                description={tool.description}
+                link={tool.link}
+              />
+            ))}
+          </div>
+        </article>
+      )}
+
+      {showSoftSkills && (
+        <article className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold underline my-6">Soft Skills:</h1>
+          <div className="text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {skillData.softSkills.map((softSkill) => (
+              <SkillCard
+                key={softSkill.title}
+                icon={softSkill.icon}
+                title={softSkill.title}
+                description={softSkill.description}
+              />
+            ))}
+          </div>
+        </article>
+      )}
     </div>
   );
 }
