c5975f47292f023894845f01a47bcb82ceb8406a
Tihomir Denev <tdenev22@abv.bg>
2024-10-08 13:44:02 +0300

    Change filter by position

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,18 @@
-import ThemeChangeIcons from '../ThemeChangeIcons/ThemeChangeIcons';
-import { DARK_THEME, LIGHT_THEME } from '../../common/constants';
-import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import ThemeChangeIcons from "../ThemeChangeIcons/ThemeChangeIcons";
+import { DARK_THEME, LIGHT_THEME } from "../../common/constants";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import {
   AiOutlineContacts,
   AiOutlineFundProjectionScreen,
   AiOutlineHome,
   AiOutlineTool,
   AiOutlineUser,
-} from 'react-icons/ai';
+} from "react-icons/ai";
 
 export default function NavBar() {
   const [theme, setTheme] = useState(
-    !localStorage.getItem('theme') ? LIGHT_THEME : localStorage.getItem('theme')
+    !localStorage.getItem("theme") ? LIGHT_THEME : localStorage.getItem("theme")
   );
 
   const handleToggle = (e) => {
@@ -20,93 +20,68 @@ export default function NavBar() {
   };
 
   useEffect(() => {
-    localStorage.setItem('theme', theme);
-    document.querySelector('html').setAttribute('data-theme', theme);
+    localStorage.setItem("theme", theme);
+    document.querySelector("html").setAttribute("data-theme", theme);
   }, [theme]);
 
   const closeDrawer = () => {
-    const drawerToggle = document.getElementById('nav-drawer');
+    const drawerToggle = document.getElementById("nav-drawer");
     if (drawerToggle) {
       drawerToggle.checked = false;
     }
   };
 
   return (
-    <div className='drawer drawer-end bg-gray-800 text-gray-300 shadow-md'>
-      <input
-        id='nav-drawer'
-        type='checkbox'
-        className='drawer-toggle'
-      />
-      <div className='flex justify-between items-center px-3'>
-        <ThemeChangeIcons
-          toggleTheme={handleToggle}
-          currentTheme={theme}
-        />
-        <div className='block md:hidden'>
-          <label
-            htmlFor='nav-drawer'
-            className='drawer-button'
-          >
+    <div className="drawer drawer-end bg-gray-800 text-gray-300">
+      <input id="nav-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="flex justify-between items-center px-3">
+        <ThemeChangeIcons toggleTheme={handleToggle} currentTheme={theme} />
+        <div className="block md:hidden">
+          <label htmlFor="nav-drawer" className="drawer-button">
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h16m-7 6h7'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
               />
             </svg>
           </label>
         </div>
-        <div className='hidden md:block'>
-          <ul className='menu menu-horizontal items-center text-lg'>
+        <div className="hidden md:block">
+          <ul className="menu menu-horizontal items-center text-lg">
             <li>
-              <NavLink
-                to='/'
-                onClick={closeDrawer}
-              >
+              <NavLink to="/" onClick={closeDrawer}>
                 <AiOutlineHome />
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to='/about'
-                onClick={closeDrawer}
-              >
+              <NavLink to="/about" onClick={closeDrawer}>
                 <AiOutlineUser />
                 About
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to='/skills'
-                onClick={closeDrawer}
-              >
+              <NavLink to="/skills" onClick={closeDrawer}>
                 <AiOutlineTool />
                 Skills
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to='/projects'
-                onClick={closeDrawer}
-              >
+              <NavLink to="/projects" onClick={closeDrawer}>
                 <AiOutlineFundProjectionScreen />
                 Projects
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to='/contact'
-                onClick={closeDrawer}
-              >
+              <NavLink to="/contact" onClick={closeDrawer}>
                 <AiOutlineContacts />
                 Contact
               </NavLink>
@@ -114,53 +89,35 @@ export default function NavBar() {
           </ul>
         </div>
       </div>
-      <div className='drawer-side z-50'>
-        <label
-          htmlFor='nav-drawer'
-          className='drawer-overlay'
-        ></label>
-        <ul className='menu p-4 overflow-y-auto w-52 rounded-2xl bg-base-100 text-lg text-gray-600'>
+      <div className="drawer-side z-50">
+        <label htmlFor="nav-drawer" className="drawer-overlay"></label>
+        <ul className="menu p-4 overflow-y-auto w-52 rounded-2xl bg-base-100 text-lg text-gray-600">
           <li>
-            <NavLink
-              to='/'
-              onClick={closeDrawer}
-            >
+            <NavLink to="/" onClick={closeDrawer}>
               <AiOutlineHome />
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to='/about'
-              onClick={closeDrawer}
-            >
+            <NavLink to="/about" onClick={closeDrawer}>
               <AiOutlineUser />
               About
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to='/skills'
-              onClick={closeDrawer}
-            >
+            <NavLink to="/skills" onClick={closeDrawer}>
               <AiOutlineTool />
               Skills
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to='/projects'
-              onClick={closeDrawer}
-            >
+            <NavLink to="/projects" onClick={closeDrawer}>
               <AiOutlineFundProjectionScreen />
               Projects
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to='/contact'
-              onClick={closeDrawer}
-            >
+            <NavLink to="/contact" onClick={closeDrawer}>
               <AiOutlineContacts />
               Contact
             </NavLink>
--- src/views/Skills/Skills.jsx
+++ src/views/Skills/Skills.jsx
@@ -30,84 +30,99 @@ export default function Skills() {
   };
 
   return (
-    <div className="flex flex-col items-center m-8 md:px-0">
-      <ul className="flex flex-row menu bg-gray-800 text-gray-300 text-xl rounded-xl w">
-        <li className="menu-title text-gray-400 text-xl">Filter By:</li>
-        <li>
-          <button onClick={() => handleToggleCategory("professional")}>
-            Skill Set
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleToggleCategory("tools")}>Tools</button>
-        </li>
-        <li>
-          <button onClick={() => handleToggleCategory("softSkills")}>
-            Soft Skills
-          </button>
-        </li>
-        <li>
-          <button
-            className="hover:bg-gray-500"
-            onClick={handleClearFilters}
-            title="Click to remove all filters and show all skills"
-          >
-            Show All
-          </button>
-        </li>
+    <div>
+      <ul className="flex flex-row items-center justify-between bg-gray-800 text-white text-md w-full py-2 px-4">
+        <li className="text-gray-400">Filter By:</li>
+        <div className="flex flex-row space-x-2">
+          <li>
+            <button
+              className="px-3 py-1 rounded hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+              onClick={() => handleToggleCategory("professional")}
+            >
+              Skills
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-3 py-1 rounded hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+              onClick={() => handleToggleCategory("tools")}
+            >
+              Tools
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-3 py-1 rounded hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+              onClick={() => handleToggleCategory("softSkills")}
+            >
+              Soft Skills
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-3 py-1 rounded hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+              onClick={handleClearFilters}
+              title="Click to remove all filters and show all skills"
+            >
+              Show All
+            </button>
+          </li>
+        </div>
       </ul>
 
-      {showProfessional && (
-        <article className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold underline my-6">
-            Professional Skill Set:
-          </h1>
-          <div className="text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {skillData.professionalSkills.map((skill) => (
-              <SkillCard
-                key={skill.title}
-                icon={skill.icon}
-                title={skill.title}
-                description={skill.description}
-                link={skill.link}
-              />
-            ))}
-          </div>
-        </article>
-      )}
+      <div className="flex flex-col items-center md:px-0">
+        {showProfessional && (
+          <article className="flex flex-col items-center">
+            <h1 className="text-2xl font-bold underline my-6">
+              Professional Skill Set:
+            </h1>
+            <div className="text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {skillData.professionalSkills.map((skill) => (
+                <SkillCard
+                  key={skill.title}
+                  icon={skill.icon}
+                  title={skill.title}
+                  description={skill.description}
+                  link={skill.link}
+                />
+              ))}
+            </div>
+          </article>
+        )}
 
-      {showTools && (
-        <article className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold underline my-6">Tools I Use:</h1>
-          <div className="text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {skillData.tools.map((tool) => (
-              <SkillCard
-                key={tool.title}
-                icon={tool.icon}
-                title={tool.title}
-                description={tool.description}
-                link={tool.link}
-              />
-            ))}
-          </div>
-        </article>
-      )}
+        {showTools && (
+          <article className="flex flex-col items-center">
+            <h1 className="text-2xl font-bold underline my-6">Tools I Use:</h1>
+            <div className="text-5xl text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {skillData.tools.map((tool) => (
+                <SkillCard
+                  key={tool.title}
+                  icon={tool.icon}
+                  title={tool.title}
+                  description={tool.description}
+                  link={tool.link}
+                />
+              ))}
+            </div>
+          </article>
+        )}
 
-      {showSoftSkills && (
-        <article className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold underline my-6">Soft Skills:</h1>
-          <div className="text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {skillData.softSkills.map((softSkill) => (
-              <SkillCard
-                key={softSkill.title}
-                icon={softSkill.icon}
-                title={softSkill.title}
-                description={softSkill.description}
-              />
-            ))}
-          </div>
-        </article>
-      )}
+        {showSoftSkills && (
+          <article className="flex flex-col items-center">
+            <h1 className="text-2xl font-bold underline my-6">Soft Skills:</h1>
+            <div className="text-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {skillData.softSkills.map((softSkill) => (
+                <SkillCard
+                  key={softSkill.title}
+                  icon={softSkill.icon}
+                  title={softSkill.title}
+                  description={softSkill.description}
+                />
+              ))}
+            </div>
+          </article>
+        )}
+      </div>
     </div>
   );
 }
